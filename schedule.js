5b9be2dc5819c9a26cc7473803ecdfa1caf1c5d4
Rohit <rohityadav2604@gmail.com>
2021-06-02 16:49:09 +0530

    Done

---
+++ schedule.js
@@ -0,0 +1,29 @@
+const cron = require('node-cron');
+const{takingclass} = require('./main.js');
+
+
+
+async function getcredential(meetlink , email , pw )
+{
+  console.log("i am in driver function");
+  //const task = cron.schedule('*/1 * * * *', async() => {
+          
+ 
+    await takingclass(meetlink , email , pw);
+    console.log("i am done with class");
+    //task.stop();
+    
+ //});
+ //task.start();
+ 
+}
+
+
+
+
+
+  
+console.log("i am starting");
+module.exports = {
+  getcredential : getcredential
+}
